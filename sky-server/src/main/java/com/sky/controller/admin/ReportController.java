e1ba3f71f617fe5998e5f52cfbc3413b84733e53
Steve95271 <124816688+Steve95271@users.noreply.github.com>
2024-05-03 13:27:51 +0800

    导出数据代码

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @RestController
@@ -89,6 +90,15 @@ public class ReportController {
     ) {
         log.info("销售数据统计，时间段{} - {}", begin, end);
         return Result.success(reportService.getSalesStatistics(begin, end));
+    }
+
+    /**
+     * 导出最近三十天的数据
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse response) {
+        reportService.exportBusinessData(response);
     }
 
 }
